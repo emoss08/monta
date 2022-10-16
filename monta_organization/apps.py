eb26ecbe3e3e251a114345e710876c8fd4f94140
Eric Moss <yeswolfprod@gmail.com>
2022-10-15 20:34:00 -0400

    Add signals and remove signals

--- monta_organization/apps.py
+++ monta_organization/apps.py
@@ -5,3 +5,6 @@ from django.apps import AppConfig
 class MontaOrganizationConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "monta_organization"
+
+    def ready(self):
+        from monta_organization import signals
